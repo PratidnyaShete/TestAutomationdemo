7bdf5129ec8a71ed17c75b8ae23765196e6041a0
PratidnyaShete <shetepratidnya5@gmail.com>
2022-07-10 12:41:01 +0530

    Initial project setup

---
+++ src/test/java/com/qa/automation/runners/TestRunner.java
@@ -0,0 +1,22 @@
+package com.qa.automation.runners;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "classpath:features",
+		glue="com.qa.automation.stepdefs",
+		tags = "",
+		plugin = {"pretty",
+				"html:target/html/htmlReport.html",
+				"json:target/json/jsonReport.json"
+		},
+		monochrome=true,
+		publish=true,
+		dryRun=false	
+		)
+public class TestRunner {
+
+}
